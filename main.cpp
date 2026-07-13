372572880558276d4d229ada4c22296e5d697f68
Sebastián Vilchez <scvilchez@gmail.com>
2026-07-13 19:07:50 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,17 +1,28 @@
 #include "OBSManager.h"
 #include <iostream>
 
-int main() {
+
+int main()
+{
     OBSManager obs;
 
-    // Fase 1: Iniciar el motor
-    if (!obs.initialize()) {
+
+    if (!obs.initialize())
+    {
         return 1;
     }
 
-    // El motor estara encendido hasta que el objeto 'obs' salga de contexto o se cierre el programa
-    std::cout << "Presiona Enter para cerrar el backend..." << std::endl;
+
+    obs.listSources();
+
+
+    std::cout
+        << "ENTER para salir..."
+        << std::endl;
+
+
     std::cin.get();
+
 
     return 0;
 }
