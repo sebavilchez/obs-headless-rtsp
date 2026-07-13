dd6deae1594196ee5533cc79f69423b05a1e380c
Sebastián Vilchez <scvilchez@gmail.com>
2026-07-13 19:08:33 -0300

    Update OBSManager.h

--- OBSManager.h
+++ OBSManager.h
@@ -15,12 +15,10 @@ public:
     void listSources();
 
 private:
-
     HMODULE hObs = nullptr;
 
     const std::string binPath = "C:\\ProyectoOBS\\bin\\64bit\\";
     const std::string dllPath = binPath + "obs.dll";
-
 
     typedef bool (*obs_startup_t)(const char*, void*, void*);
     typedef void (*obs_shutdown_t)(void);
@@ -28,21 +26,11 @@ private:
     typedef void (*obs_load_all_modules_t)(void);
     typedef void (*obs_post_load_modules_t)(void);
 
-
-    typedef bool (*obs_enum_source_types_t)(
-        size_t idx,
-        const char **type,
-        const char **display_name
-    );
-
+    typedef void (*obs_enum_sources_t)(void);
 
     obs_shutdown_t obs_shutdown = nullptr;
-
     obs_load_all_modules_t obs_load_all_modules = nullptr;
-
     obs_post_load_modules_t obs_post_load_modules = nullptr;
-
-    obs_enum_source_types_t obs_enum_source_types = nullptr;
-1};
+};
 
 #endif
